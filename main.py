fbc5a5d9f2b523b723f2b86003f4694ef4df97dc
jorge-armando-navarro-flores <jorge.armando.navarro.flores@gmail.com>
2022-02-16 12:48:17 -0600

    posts

--- main.py
+++ main.py
@@ -2,26 +2,32 @@ from flask import Flask, render_template
 from post import Post
 import requests
 
-posts = requests.get("https://api.npoint.io/eef34c0eb71ea8b95cd3").json()
+posts = requests.get("https://api.npoint.io/2c5f72cebe7064305468").json()
 post_objects = []
 for post in posts:
-    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"], post["date"])
     post_objects.append(post_obj)
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def get_all_posts():
+def get_home():
     return render_template("index.html", all_posts=post_objects)
+
 
 @app.route('/about')
 def get_about():
     return render_template("about.html")
 
 
+@app.route("/contact")
+def get_contact():
+    return render_template("contact.html")
+
+
 @app.route("/post/<int:index>")
-def show_post(index):
+def get_post(index):
     requested_post = None
     for blog_post in post_objects:
         if blog_post.id == index:
--- post.py
+++ post.py
@@ -1,6 +1,7 @@
 class Post:
-    def __init__(self, post_id, title, subtitle, body):
+    def __init__(self, post_id, title, subtitle, body, date):
         self.id = post_id
         self.title = title
         self.subtitle = subtitle
         self.body = body
+        self.date = date
